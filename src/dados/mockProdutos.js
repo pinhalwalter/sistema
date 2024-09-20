1b1b2e804eb4b3e6235c42d2676966e239d275ab
pinhalwalter <walterpinhal250@gmail.com>
2024-09-20 08:36:37 -0300

    Alteração Concluida

--- src/dados/mockProdutos.js
+++ src/dados/mockProdutos.js
@@ -2,18 +2,18 @@ export const produtos = [
     {
         "codigo":1,
         "descricao":"Camiseta",
-        "precoCusto":10.00,
-        "precoVenda":20.00,
-        "qtdEstoque":100,
+        "precoCusto":15.00,
+        "precoVenda":30.00,
+        "qtdEstoque":150,
         "urlImagem":"",
         "dataValidade":"31/12/2999"
     },
     {
         "codigo":2,
         "descricao":"blusa de algodão",
-        "precoCusto":18.00,
-        "precoVenda":36.00,
-        "qtdEstoque":50,
+        "precoCusto":36.00,
+        "precoVenda":83.00,
+        "qtdEstoque":25,
         "urlImagem":"",
         "dataValidade":"31/12/2999"
     }
