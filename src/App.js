4c82f5323855eadc813a6983ba3b91ec1ea79efb
pinhalwalter <walterpinhal250@gmail.com>
2024-09-26 13:19:39 -0300

    Cadastro/Alteração de Entregador Finalizado

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import TelaCadastroUsuario from "./componentes/Telas/TelaCadastroUsuario";
 import TelaMenu from "./componentes/Telas/TelaMenu";
 import Tela404 from "./componentes/Telas/Tela404";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import TelaCadastroEntregador from "./componentes/Telas/TelaCadastroEntregadores";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/produto" element={<TelaCadastroProduto />} />
           <Route path="/categoria" element={<TelaCadastroCategoria />} />
           <Route path="/usuario" element={<TelaCadastroUsuario/>}/>
+          <Route path="/entregadores" element={<TelaCadastroEntregador/>}/>
           <Route path="/" element={<TelaMenu />} />
           <Route path="*" element={<Tela404 />} />
         </Routes>
